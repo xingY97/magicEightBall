c4d36a8b219da684844e7ab1cb2d6fab1c7c7d2c
Xing_Ye <YOUR_PERSONAL_EMAIL@example.com>
2020-02-13 13:43:29 -0800

    random function implemented

--- MagicEightBall/ViewController.swift
+++ MagicEightBall/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //Mark: - Properties
+    
+    let answers = ["Yes, definitely", "it is certain", "Without a doubt", "Yes", "Most likely", "Sure, why not?", "Same", "Tell me more", "Out to lunch", "Reply hazy, try again", "Ask agaub later", "the cake is a lie", "42", "TMI", "Very doubtful", "Don't count on it", "My reply is no", "Absolutely not"]
+    
     @IBOutlet weak var answerLabel: UILabel!
     @IBOutlet weak var shakeButton: UIButton!
     
@@ -16,10 +21,23 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func generateAnswer() {
+        let randomIndex = Int.random(in: 0..<answers.count)
+
+        answerLabel.text = answers[randomIndex]
+    }
 
     @IBAction func shakeButtonTapped(_ sender: Any) {
-        print("Shake it like a polaroid picture!")
+        generateAnswer()
     }
-    
-}
 
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        guard motion == .motionShake else { return }
+
+        generateAnswer()
+    }
+
+    
+
+}
